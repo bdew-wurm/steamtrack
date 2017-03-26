30f45b72aa7af3ea0c73d1fb517125fb42f841a1
bdew <blackdew@gmail.com>
2017-03-26 15:15:49 +0300

    More work on tracking/banning

--- src/main/java/net/bdew/wurm/steamtracker/SteamTracker.java
+++ src/main/java/net/bdew/wurm/steamtracker/SteamTracker.java
@@ -1,20 +1,19 @@
 package net.bdew.wurm.steamtracker;
 
+import com.wurmonline.server.creatures.Communicator;
 import javassist.CannotCompileException;
 import javassist.ClassPool;
 import javassist.CtClass;
+import javassist.CtMethod;
 import javassist.expr.ExprEditor;
 import javassist.expr.MethodCall;
 import org.gotti.wurmunlimited.modloader.classhooks.HookManager;
-import org.gotti.wurmunlimited.modloader.interfaces.Initable;
-import org.gotti.wurmunlimited.modloader.interfaces.PreInitable;
-import org.gotti.wurmunlimited.modloader.interfaces.ServerStartedListener;
-import org.gotti.wurmunlimited.modloader.interfaces.WurmMod;
+import org.gotti.wurmunlimited.modloader.interfaces.*;
 
 import java.sql.SQLException;
 import java.util.logging.Logger;
 
-public class SteamTracker implements WurmMod, Initable, PreInitable, ServerStartedListener {
+public class SteamTracker implements WurmServerMod, Initable, PreInitable, ServerStartedListener, PlayerMessageListener {
     public static final Logger logger = Logger.getLogger("SteamTracker");
 
     @Override
@@ -23,7 +22,9 @@ public class SteamTracker implements WurmMod, Initable, PreInitable, ServerStart
             ClassPool classPool = HookManager.getInstance().getClassPool();
 
             CtClass ctLoginHandler = classPool.getCtClass("com.wurmonline.server.LoginHandler");
-            ctLoginHandler.getMethod("handleLogin", "(Ljava/lang/String;Ljava/lang/String;ZZZZLjava/lang/String;Ljava/lang/String;)V").instrument(new ExprEditor() {
+
+            CtMethod mHandleLogin = ctLoginHandler.getMethod("handleLogin", "(Ljava/lang/String;Ljava/lang/String;ZZZZLjava/lang/String;Ljava/lang/String;)V");
+            mHandleLogin.instrument(new ExprEditor() {
                 @Override
                 public void edit(MethodCall m) throws CannotCompileException {
                     if (m.getMethodName().equals("initialisePlayer")) {
@@ -32,6 +33,14 @@ public class SteamTracker implements WurmMod, Initable, PreInitable, ServerStart
                     }
                 }
             });
+            mHandleLogin.insertBefore(
+                    "if (net.bdew.wurm.steamtracker.SteamTrackerHook.isBanned(name, steamIDAsString)) {" +
+                            "Server.getInstance().steamHandler.removeIsPlayerAuthenticated(steamIDAsString);" +
+                            "Server.getInstance().steamHandler.EndAuthSession(steamIDAsString);" +
+                            "this.sendLoginAnswer(false, \"You are banned.\", 0.0f, 0.0f, 0.0f, 0.0f, 0, \"model.player.broken\", (byte)0, 0);" +
+                            "return;" +
+                            "};"
+            );
 
         } catch (Throwable e) {
             throw new RuntimeException(e);
@@ -49,5 +58,15 @@ public class SteamTracker implements WurmMod, Initable, PreInitable, ServerStart
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    @Override
+    public boolean onPlayerMessage(Communicator communicator, String s) {
+        return false;
+    }
+
+    @Override
+    public MessagePolicy onPlayerMessage(Communicator communicator, String message, String title) {
+        return null;
     }
 }
